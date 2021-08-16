bbe576c75e8572bd3167eb8ff0a7e477b413bcf8
Dũng <dungnh91@gmail.com>
2021-08-16 16:02:59 +0700

    add authentication

--- scan.sh
+++ scan.sh
@@ -9,6 +9,11 @@ if [ -z "$MOBSF_API_KEY" ]; then
   exit 126
 fi
 
+if [ -z "$AUTH" ]; then
+  echo "AUTH is required to run MobSF action. (AUTH = $AUTH)"
+  exit 126
+fi
+
 if [ -z "$INPUT_FILE_NAME" ]; then
   echo "INPUT_FILE_NAME is required to run MobSF action. (INPUT_FILE_NAME = $INPUT_FILE_NAME)"
   exit 126
@@ -46,17 +51,17 @@ echo "[/api/v1/upload] Received: FILE_NAME=${FILE_NAME}, HASH=${HASH}, SCAN_TYPE
 
 # Start the scan.
 echo "[/api/v1/scan] Start the scan"
-curl -X POST --url ${MOBSF_URL}/api/v1/scan --data "scan_type=${SCAN_TYPE}&file_name=${FILE_NAME}&hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}" -s -S --output tmp.json
+curl -X POST --url ${MOBSF_URL}/api/v1/scan --data "scan_type=${SCAN_TYPE}&file_name=${FILE_NAME}&hash=${HASH}" -H "Authorization:${AUTH}" -H "X-Mobsf-Api-Key:${MOBSF_API_KEY}" -s -S --output tmp.json
 echo "[/api/v1/scan] Scan finisehd"
 
 # Generate the json report.
 echo "[/api/v1/report_json] Generate the json report"
-curl -X POST --url ${MOBSF_URL}/api/v1/report_json --data "hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}" --output ${OUTPUT_FILE_NAME}.json -s -S
+curl -X POST --url ${MOBSF_URL}/api/v1/report_json --data "hash=${HASH}" -H "Authorization:${AUTH}" -H "X-Mobsf-Api-Key:${MOBSF_API_KEY}" --output ${OUTPUT_FILE_NAME}.json -s -S
 echo "[/api/v1/report_json] JSON report generated"
 
 # Generate the pdf report.
 echo "[/api/v1/download_pdf] Generate the PDF report"
-curl -X POST --url ${MOBSF_URL}/api/v1/download_pdf --data "hash=${HASH}" -H "Authorization:${MOBSF_API_KEY}" --output ${OUTPUT_FILE_NAME}.pdf -s -S
+curl -X POST --url ${MOBSF_URL}/api/v1/download_pdf --data "hash=${HASH}" -H "Authorization:${AUTH}" -H "X-Mobsf-Api-Key:${MOBSF_API_KEY}" --output ${OUTPUT_FILE_NAME}.pdf -s -S
 echo "[/api/v1/download_pdf] PDF report generated"
 
 # Remove analysis from MobSF server.
